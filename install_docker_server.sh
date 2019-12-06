c22cd75f55922a86a782b8c0b4acf1713df7678d
rootly <48964029+rootly-be@users.noreply.github.com>
2019-12-06 14:38:40 +0100

    Create install_docker_server.sh

---
+++ install_docker_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## INSTALL VIRTUALBOX
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+apt-get install linux-headers-$(uname -r) dkms -y
+apt-get install virtualbox-6.0 -y
+cd ~/
+wget https://download.virtualbox.org/virtualbox/6.0.0/Oracle_VM_VirtualBox_Extension_Pack-6.0.0.vbox-extpack
+echo "y" | vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.0.0.vbox-extpack
+
+##INSTALL DOCKER
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl enable docker
+
+## INSTALL DOCKER-MACHINE
+base=https://github.com/docker/machine/releases/download/v0.16.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
+mv /tmp/docker-machine /usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
